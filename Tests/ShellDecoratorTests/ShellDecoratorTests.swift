2def1e57b8bd9cf9ffe1a52a3739a46231650609
417-72KI <417.72ki@gmail.com>
2023-10-15 17:03:05 +0900

    add test pattern and sample pattern

--- Tests/ShellDecoratorTests/ShellDecoratorTests.swift
+++ Tests/ShellDecoratorTests/ShellDecoratorTests.swift
@@ -4,9 +4,12 @@ import XCTest
 final class ColorFuncTests: XCTestCase {
     func testBuild() throws {
         let pattern: [(ColorFunc, String, StaticString, UInt)] = [
-            (.default, "\u{001B}[m", #file, #line)
+            (.default, "\u{001B}[m", #file, #line),
+            (.red, "\u{001B}[31m", #file, #line),
+            (.green.backgroundYellow, "\u{001B}[32;43m", #file, #line),
+            (.blue.backgroundMagenta.bold, "\u{001B}[34;45;1m", #file, #line),
+            (.white.backgroundCyan.bold.underline, "\u{001B}[37;46;1;4m", #file, #line),
         ]
-
         pattern.forEach { cs, expected, file, line in
             XCTAssertEqual(expected, cs.build(), file: file, line: line)
         }
