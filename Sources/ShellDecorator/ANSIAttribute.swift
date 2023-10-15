11c3ee361d319616eedd252f8d4990543a8d6918
417-72KI <417.72ki@gmail.com>
2023-10-15 16:56:44 +0900

    move extensions

--- Sources/ShellDecorator/ANSIAttribute.swift
+++ Sources/ShellDecorator/ANSIAttribute.swift
@@ -14,3 +14,19 @@ public enum ANSIAttribute: Int, CaseIterable {
     /// 非表示
     case invisibleText = 8
 }
+
+// MARK: - ColorFunc extensions
+public extension ColorFunc {
+    /// 太字
+    var bold: Self { appending(attribute: .bold) }
+    /// 弱強調
+    var lowIntensity: Self { appending(attribute: .lowIntensity) }
+    /// 下線
+    var underline: Self { appending(attribute: .underline) }
+    /// 点滅
+    var blink: Self { appending(attribute: .blink) }
+    /// 反転
+    var reverseVideo: Self { appending(attribute: .reverseVideo) }
+    /// 非表示
+    var invisibleText: Self { appending(attribute: .invisibleText) }
+}
--- Sources/ShellDecorator/ANSIColor.swift
+++ Sources/ShellDecorator/ANSIColor.swift
@@ -10,3 +10,37 @@ public enum ANSIColor: Int, CaseIterable {
     case cyan = 6
     case white = 7
 }
+
+// MARK: - ColorFunc extensions
+public extension ColorFunc {
+    static var black: Self { .init(textColor: .black) }
+    static var red: Self { .init(textColor: .red) }
+    static var green: Self { .init(textColor: .green) }
+    static var yellow: Self { .init(textColor: .yellow) }
+    static var blue: Self { .init(textColor: .blue) }
+    static var magenta: Self { .init(textColor: .magenta) }
+    static var cyan: Self { .init(textColor: .cyan) }
+    static var white: Self { .init(textColor: .white) }
+}
+
+public extension ColorFunc {
+    static var backgroundBlack: Self { .init(backgroundColor: .black) }
+    static var backgroundRed: Self { .init(backgroundColor: .red) }
+    static var backgroundGreen: Self { .init(backgroundColor: .green) }
+    static var backgroundYellow: Self { .init(backgroundColor: .yellow) }
+    static var backgroundBlue: Self { .init(backgroundColor: .blue) }
+    static var backgroundMagenta: Self { .init(backgroundColor: .magenta) }
+    static var backgroundCyan: Self { .init(backgroundColor: .cyan) }
+    static var backgroundWhite: Self { .init(backgroundColor: .white) }
+}
+
+public extension ColorFunc {
+    var backgroundBlack: Self { mutating(\.backgroundColor, .black) }
+    var backgroundRed: Self { mutating(\.backgroundColor, .red) }
+    var backgroundGreen: Self { mutating(\.backgroundColor, .green) }
+    var backgroundYellow: Self { mutating(\.backgroundColor, .yellow) }
+    var backgroundBlue: Self { mutating(\.backgroundColor, .blue) }
+    var backgroundMagenta: Self { mutating(\.backgroundColor, .magenta) }
+    var backgroundCyan: Self { mutating(\.backgroundColor, .cyan) }
+    var backgroundWhite: Self { mutating(\.backgroundColor, .white) }
+}
